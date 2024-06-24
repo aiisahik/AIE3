110d1edb8ca6a03df794b4dd3ce0d8fde8ee110b
aiisahik <jie.z.zhou@gmail.com>
2024-06-24 15:07:19 +0200

    added MarkdownTextSplitter

--- Week 4/Day 1/app.py
+++ Week 4/Day 1/app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from operator import itemgetter
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_core.prompts import PromptTemplate
@@ -64,7 +64,7 @@ llama_parse_documents = llama_parser.load_data("./data/airbnb10k-2024q1.pdf")
 documents = [doc.to_langchain_format() for doc in llama_parse_documents]
 
 ### 2. CREATE TEXT SPLITTER AND SPLIT DOCUMENTS
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=30)
+text_splitter = MarkdownTextSplitter(chunk_size=1000, chunk_overlap=30)
 split_documents = text_splitter.split_documents(documents)
 
 ### 3. LOAD HUGGINGFACE EMBEDDINGS
